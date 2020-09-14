b43211f01a64581962b535c66f46e7898745a3a5
Justin Leach <leachjustin18@yahoo.com>
2020-09-14 13:11:27 -0500

    Fix spelling error

--- learning/src/com/justinLeach/Main.java
+++ learning/src/com/justinLeach/Main.java
@@ -7,8 +7,8 @@ public class Main {
         long viewsCount = 4_486_481_456L;
         float price = 12.99F;
         char letter = 'A';
-        boolean isMontized = true;
+        boolean isMonetized = true;
 
-        System.out.println("Youtubers age " + age + ", youtube views " + viewsCount + ", cost to make video " + price + ", grade of most videos " + letter + ", and videos are monetized " + isMontized);
+        System.out.println("Youtubers age " + age + ", youtube views " + viewsCount + ", cost to make video " + price + ", grade of most videos " + letter + ", and videos are monetized " + isMonetized);
     }
 }
