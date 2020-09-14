c3bb7068e3fa495d5f816250f093af08172f638a
Justin Leach <leachjustin18@yahoo.com>
2020-09-14 13:10:53 -0500

    Working with various primitive types

--- learning/src/com/justinLeach/Main.java
+++ learning/src/com/justinLeach/Main.java
@@ -3,8 +3,12 @@ package com.justinLeach;
 public class Main {
 
     public static void main(String[] args) {
-        int age = 30;
-        String name = "John Doe";
-        System.out.println("Hello my name is " + name + " and my age is "  + age);
+        byte age = 52;
+        long viewsCount = 4_486_481_456L;
+        float price = 12.99F;
+        char letter = 'A';
+        boolean isMontized = true;
+
+        System.out.println("Youtubers age " + age + ", youtube views " + viewsCount + ", cost to make video " + price + ", grade of most videos " + letter + ", and videos are monetized " + isMontized);
     }
 }
