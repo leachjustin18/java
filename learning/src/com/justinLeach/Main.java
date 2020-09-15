c3f92c71918d40cc3ff6a11304ea4d758f16a5b7
Justin Leach <leachjustin18@yahoo.com>
2020-09-15 13:09:43 -0500

    Add local object

--- learning/src/com/justinLeach/Main.java
+++ learning/src/com/justinLeach/Main.java
@@ -4,15 +4,21 @@ import java.util.Date;
 
 public class Main {
 
-    public Main(String name, String age, String type) {
-        System.out.println("My animal name is " + name + " and it is " + age + " old and it's a " + type);
+    public Main(String name, byte age) {
+        System.out.println("My name " + name + " and I am " + age + " year old");
     }
 
-    public static void main(String[] args) {
+    private static String Animal(String name, String age, String type) {
+        return "My animal's name is " + name + " and it is " + age + " old and it's a " + type;
+    }
+
+    public static void main (String[] args) {
         Date now = new Date();
         System.out.println("Date " + now);
         long timeString = now.getTime();
         System.out.println("Time string " + timeString);
-        final Main main = new Main("Timothy", "6 months", "dog");
+        new Main("Justin", (byte) 30);
+        var animal = Animal("Timothy", "6 months", "dog");
+        System.out.println(animal);
     }
 }
