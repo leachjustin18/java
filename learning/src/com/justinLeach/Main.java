4a937483433f08f82e06b767e98cb657ad6d1b88
Justin Leach <leachjustin18@yahoo.com>
2020-09-16 12:50:26 -0500

    Added different reference types

--- learning/src/com/justinLeach/Main.java
+++ learning/src/com/justinLeach/Main.java
@@ -1,5 +1,6 @@
 package com.justinLeach;
 
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
@@ -7,18 +8,26 @@ public class Main {
     public Main(String name, byte age) {
         System.out.println("My name " + name + " and I am " + age + " year old");
     }
-
+    
     private static String Animal(String name, String age, String type) {
         return "My animal's name is " + name + " and it is " + age + " old and it's a " + type;
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Date now = new Date();
         System.out.println("Date " + now);
+
         long timeString = now.getTime();
         System.out.println("Time string " + timeString);
+
         new Main("Justin", (byte) 30);
-        var animal = Animal("Timothy", "6 months", "dog");
+
+        String animal = Animal("Timothy", "6 months", "dog");
         System.out.println(animal);
+
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
