54d003d14a4663baebd3004aac128d7e23e0d06b
Justin Leach <leachjustin18@yahoo.com>
2020-09-16 13:08:20 -0500

    Standard escape sequences

--- learning/src/com/justinLeach/Main.java
+++ learning/src/com/justinLeach/Main.java
@@ -3,20 +3,16 @@ package com.justinLeach;
 public class Main {
 
     public static void main(String[] args) {
-        String messageOldWay = new String("old way");
-        System.out.println("Old way string " + messageOldWay);
+       String message = "Hello \"You\"";
+       System.out.println(message);
 
-        //Standard way of determining string
-        String message = "   Standard Message  ";
-        System.out.println("Standard way " + message);
-        System.out.println("Ends with E " + message.endsWith("e"));
-        System.out.println("Starts with E " + message.startsWith("e"));
-        System.out.println("Uppercase " + message.toUpperCase());
-        System.out.println("Length " + message.length());
-        System.out.println("Index of " + message.indexOf("n"));
-        System.out.println("Index of not found " + message.indexOf("z"));
-        System.out.println("Replace " + message.replace("s", "c"));
-        System.out.println("Lowercase " + message.toLowerCase());
-        System.out.println("Trim " + message.trim());
+       String backSlash = "c:\\Windows\\...";
+       System.out.println(backSlash);
+
+        String newLine = "c:\nWindows\\...";
+        System.out.println(newLine);
+
+        String tab = "c:\tWindows\\...";
+        System.out.println(tab);
     }
 }
