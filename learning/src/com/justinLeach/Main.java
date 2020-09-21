212b7c989a413c3e4e3a0ad2275dd5d6ac80a818
Justin Leach <leachjustin18@yahoo.com>
2020-09-21 12:53:23 -0500

    Various examples for arrays

--- learning/src/com/justinLeach/Main.java
+++ learning/src/com/justinLeach/Main.java
@@ -1,18 +1,28 @@
 package com.justinLeach;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-       String message = "Hello \"You\"";
-       System.out.println(message);
+        // Arrays have a fixed length when defined
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        //Prints address of array
+        System.out.println("Address of array " + numbers);
 
-       String backSlash = "c:\\Windows\\...";
-       System.out.println(backSlash);
+        //Print values of array
+        System.out.println("To string " + Arrays.toString(numbers));
 
-        String newLine = "c:\nWindows\\...";
-        System.out.println(newLine);
+        int[] numbersDefined = {2, 3, 4, 5, 1};
 
-        String tab = "c:\tWindows\\...";
-        System.out.println(tab);
+        System.out.println("Array defined");
+        System.out.println(Arrays.toString(numbersDefined));
+        System.out.println("Length " + numbersDefined.length);
+
+        //Sort
+        Arrays.sort(numbersDefined);
+        System.out.println("Sorted " + Arrays.toString(numbersDefined));
     }
 }
