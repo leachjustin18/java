d6c36c905e38674d6c99a6522213d1f99cd57526
Justin Leach <leachjustin18@yahoo.com>
2020-09-22 12:49:07 -0500

    Add constant example and comment

--- learning/src/com/justinLeach/Main.java
+++ learning/src/com/justinLeach/Main.java
@@ -1,16 +1,12 @@
 package com.justinLeach;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
+        // final makes it a const
+        //Uppercase constants(final)
+        final float PI = 3.14f;
 
-        int[][] numbers = new int[2][3];
-        numbers[0][0] = 1;
-        System.out.println(Arrays.deepToString(numbers));
-
-        int[][] multiDimensionInit = {{1, 2, 3}, {4, 5, 6}};
-        System.out.println(Arrays.deepToString(multiDimensionInit));
+        System.out.println(PI);
     }
 }
