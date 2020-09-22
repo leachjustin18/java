d8460356d371334af7bca8499f4cc109f45b6ead
Justin Leach <leachjustin18@yahoo.com>
2020-09-22 12:45:17 -0500

    Add multiDimension array examples

--- learning/src/com/justinLeach/Main.java
+++ learning/src/com/justinLeach/Main.java
@@ -5,24 +5,12 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        // Arrays have a fixed length when defined
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        //Prints address of array
-        System.out.println("Address of array " + numbers);
 
-        //Print values of array
-        System.out.println("To string " + Arrays.toString(numbers));
+        int[][] numbers = new int[2][3];
+        numbers[0][0] = 1;
+        System.out.println(Arrays.deepToString(numbers));
 
-        int[] numbersDefined = {2, 3, 4, 5, 1};
-
-        System.out.println("Array defined");
-        System.out.println(Arrays.toString(numbersDefined));
-        System.out.println("Length " + numbersDefined.length);
-
-        //Sort
-        Arrays.sort(numbersDefined);
-        System.out.println("Sorted " + Arrays.toString(numbersDefined));
+        int[][] multiDimensionInit = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(Arrays.deepToString(multiDimensionInit));
     }
 }
