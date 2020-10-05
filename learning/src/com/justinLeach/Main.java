5a8cbd6d8842d053389fc387b1a4fedc0de760c7
Justin Leach <leachjustin18@yahoo.com>
2020-10-05 13:45:59 -0500

    Add examples for math class

--- learning/src/com/justinLeach/Main.java
+++ learning/src/com/justinLeach/Main.java
@@ -3,20 +3,32 @@ package com.justinLeach;
 public class Main {
 
     public static void main(String[] args) {
-        //Implicit casting
-        // byte > short > int > long > float > double
-        short x = 1;
-        int y = x + 2;
-        System.out.println("Y " + y);
+        //F casts to float
+        int round = Math.round(1.1F);
+        System.out.println("Round " + round);
 
-        double x2 = 1.1;
-        double y2 = x2 + 2; // 2 becomes 2.0
-        System.out.println("Y2 " + y2);
+        int ceil = (int)Math.ceil(2.1F);
+        System.out.println("Ceil " + ceil);
 
-        //Explicit casing
+        int floor = (int)Math.floor(2.6F);
+        System.out.println("Floor " + floor);
 
-        double x3 = 1.1;
-        int y3 = (int)(x3) + 2;
-        System.out.println("Y3 " + y3);
+        int max = Math.max(10, 22); // same concept for min
+        System.out.println("Max " + max);
+
+        double random = Math.random();
+        System.out.println("Random " + random);
+
+        double randomAbove1 = Math.random() * 100;
+        System.out.println("Random Above 0 " + randomAbove1);
+
+        double randomAbove1Round = Math.round(Math.random() * 100);
+        System.out.println("Random Above 0 Round " + randomAbove1Round);
+
+        byte randomAbove1RoundByte = (byte)Math.round(Math.random() * 100);
+        System.out.println("Random Above 0 Round Byte " + randomAbove1RoundByte);
+
+        byte randomAbove1Byte = (byte)(Math.random() * 100);
+        System.out.println("Random Above 0 Byte " + randomAbove1Byte);
     }
 }
