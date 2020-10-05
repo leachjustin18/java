c20987217f13e603963a9cf9e381e9c6cb9faca1
Justin Leach <leachjustin18@yahoo.com>
2020-10-05 13:31:20 -0500

    Added examples for casting

--- learning/src/com/justinLeach/Main.java
+++ learning/src/com/justinLeach/Main.java
@@ -3,10 +3,20 @@ package com.justinLeach;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 10 + 2 * 3;
-        System.out.println("Multiple " + x);
+        //Implicit casting
+        // byte > short > int > long > float > double
+        short x = 1;
+        int y = x + 2;
+        System.out.println("Y " + y);
 
-        int y = (10 + 2) * 3;
-        System.out.println("Parenthesis " + y);
+        double x2 = 1.1;
+        double y2 = x2 + 2; // 2 becomes 2.0
+        System.out.println("Y2 " + y2);
+
+        //Explicit casing
+
+        double x3 = 1.1;
+        int y3 = (int)(x3) + 2;
+        System.out.println("Y3 " + y3);
     }
 }
